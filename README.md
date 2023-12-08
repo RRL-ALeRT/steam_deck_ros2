a139df611e414bf269f08ebcd2a4a44b3073e80e
Shubham <skpawar1305@gmail.com>
2023-12-08 16:08:37 +0100

    sync non ros files

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 Thanks to https://github.com/ctu-vras/steam-deck-ros-controller
 You may follow the initial setup, but just install ROS2 Humble using Robostack
+
+## Setup
+
+Copy spot_controls_toggle.py and spot.png to home directory.
+
+
+Add following to crontab -e
+```
+@reboot sleep 10 && python3 /home/deck/spot_controls_toggle.py
+```
+
+## Only clone/copy msgs and description packages and spot_driver_plus from
+```
+https://github.com/RRL-ALeRT/alert_ros2/tree/nuc/spot_driver_plus
+```
---
+++ spot_controls_toggle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+# For steam deck, this has to be executed through crontab @reboot
+# All the dependencies should be installed in arch
+# sudo pacman -Sy pango python-gobject gtk3 libappindicator-gtk3
+
+import sys
+import subprocess
+import signal
+from PyQt5.QtWidgets import QApplication
+import gi
+gi.require_version('AppIndicator3', '0.1')
+from gi.repository import AppIndicator3 as appindicator, Gtk
+import time
+import subprocess
+
+
+def open_ros_apps():
+    commands = [
+        # "killall steam", # else controller doesn't work
+        "screen -dmS deck"
+    ]
+    for command in commands:
+        subprocess.Popen(command, shell=True)
+
+    time.sleep(1)  # Add a delay to allow the screen session to initialize
+
+    commands = {
+        "rqt": "rqt",
+        "rviz2": "rviz2",
+        "controller": "ros2 launch spot_driver_plus controller_launch.py",
+        "display": "ros2 launch image_display image_display_launch.py",
+        "deck_capture": "ros2 run audio_capture audio_capture_node --ros-args -p format:=wave -r __ns:=/operator",
+        "deck_play": "ros2 run audio_play audio_play_node --ros-args -p format:=wave -r __ns:=/nuc",
+    }
+    
+    for tab, command in commands.items():
+        subprocess.Popen(f"screen -S deck -p {tab} -X stuff \"{command}\n\"", shell=True)
+
+
+def close_ros_apps():
+    # Terminate the 'deck' screen session
+    subprocess.Popen("screen -S deck -X quit", shell=True)
+
+
+def toggle_ros_apps(_):
+    global apps_running
+    if apps_running:
+        close_ros_apps()
+        apps_running = False
+    else:
+        open_ros_apps()
+        apps_running = True
+
+
+apps_running = False
+
+app = QApplication(sys.argv)
+app.setQuitOnLastWindowClosed(False)
+
+spot_icon = "/home/deck/spot.png"
+
+indicator = appindicator.Indicator.new("Spot Control View", spot_icon, appindicator.IndicatorCategory.SYSTEM_SERVICES)
+indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+
+menu = Gtk.Menu()
+
+toggle_item = Gtk.ImageMenuItem.new_with_label("Toggle Control View")
+toggle_item.set_image(Gtk.Image.new_from_file(spot_icon))
+toggle_item.set_always_show_image(True)
+toggle_item.connect('activate', toggle_ros_apps)
+menu.append(toggle_item)
+
+menu.show_all()
+indicator.set_menu(menu)
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal.SIG_DFL)  # Enable Ctrl+C handling
+    Gtk.main()
